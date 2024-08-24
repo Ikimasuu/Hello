5ff81b46d1e0270f543faeacb4bcf6d41765a754
unknown <kharl.patrick.cedeno420@gmail.com>
2024-08-24 21:56:33 +0800

    added get name function

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-name = input("What's your name? ")
-print(name)
+def main():
+    name = get_name().strip()
+    print(f"My name is {name}")
+
+
+def get_name():
+    return input("What's your name? ")
+
+main()
