c3f68bd0bcaa85c2aec8a9eb6a69d91d63dfdf46
Ikimasuu <162311262+Ikimasuu@users.noreply.github.com>
2024-08-25 06:55:15 +0800

    added get age function

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 def main():
     name = get_name().strip()
-    print(f"My name is {name}")
-
-
+    age = get_age().strip()
+    print(f"My name is {name}, I'm {age} years old")
+    
 def get_name():
     return input("What's your name? ")
 
+def get_age():
+    return input ("What's your age?")
+    
 main()
